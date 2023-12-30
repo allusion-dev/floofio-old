d8de7355b5bd3b691fd47a3e0d321ff5c06ba33a
nightsky6138 <124866443+nightsky6138@users.noreply.github.com>
2023-12-29 22:52:44 -0800

    Update startMenu.js

--- js/startMenu.js
+++ js/startMenu.js
@@ -16,18 +16,18 @@ if (!self.__WB_pmw) {
     const f = document.getElementById("changelog");
     const g = document.getElementById("loginMenu");
     const h = document.getElementById("canvas");
-    const i = h.getContext('2d', {
-      'alpha': false
+    const i = h.getContext("2d", {
+      "alpha": false
     });
     const j = function() {
       const k = {};
       for (const l of [
-          ['./resources/tiles/grass.svg', "grass"]
+          ["./resources/tiles/grass.svg", "grass"]
         ]) {
         const m = new Image();
-        m.src = l[0x0];
+        m.src = l[0];
         m.ready = true;
-        k[l[0x1]] = m;
+        k[l[1]] = m;
         m.addEventListener("load", () => m.ready = true);
       }
       return k;
@@ -49,24 +49,24 @@ if (!self.__WB_pmw) {
       let w = j.grass.height * q;
       for (let aa = -r % v; aa <= innerWidth; aa += v) {
         for (let ab = -t % v; ab <= innerHeight; ab += v) {
-          if (aa + v < 0x0 || ab + v < 0x0 || aa - v > innerWidth || ab - v > innerHeight) {
+          if (aa + v < 0 || ab + v < 0 || aa - v > innerWidth || ab - v > innerHeight) {
             continue;
           }
-          i.drawImage(j.grass, aa | 0x0, ab | 0x0, v + 0x1 | 0x0, w + 0x1 | 0x0);
+          i.drawImage(j.grass, aa | 0, ab | 0, v + 1 | 0, w + 1 | 0);
         }
       }
-      i.globalAlpha = 0x1;
+      i.globalAlpha = 1;
     }
     let ac = false;
-    let ad = Math.max(innerWidth / 0x320, innerHeight / 0x320 / 0x438 * 0x780);
-    let ae = 0x0;
-    let af = 0x0;
+    let ad = Math.max(innerWidth / 800, innerHeight / 800 / 1080 * 1920);
+    let ae = 0;
+    let af = 0;
     let ag = {
-      'x': 0x0,
-      'y': 0x0
+      "x": 0,
+      "y": 0
     };
     let ah = [];
-    let ai = 0x0;
+    let ai = 0;
     window.addEventListener("mousemove", function(aj) {
       ag.x = aj.clientX * window.devicePixelRatio;
       ag.y = aj.clientY * window.devicePixelRatio;
@@ -77,41 +77,41 @@ if (!self.__WB_pmw) {
       i.translate(am, an);
       i.rotate(ap);
       i.scale(ao * aq, ao * aq);
-      let ar = 0x40 / ao / aq;
-      let as = 0x40 / ao / aq;
-      i.drawImage(al, -ar / 0x2, -as / 0x2, ar, as);
+      let ar = 64 / ao / aq;
+      let as = 64 / ao / aq;
+      i.drawImage(al, -ar / 2, -as / 2, ar, as);
       i.restore();
     }
     let at = function() {
-      let au = 0x0;
+      let au = 0;
       let av = Object.keys(window.petalIcons).sort(() => Math.random() - 0.5);
       return function() {
-        au = (au + 0x1) % av.length;
+        au = (au + 1) % av.length;
         return window.petalIcons[av[au]];
       };
     };
 
     function aw() {
-      i.clearRect(0x0, 0x0, canvas.width, canvas.height);
-      ae = ae + 0.01 * (0x0 - ae);
-      af = af + 0.01 * (0x0 - af);
-      p(ad, ae + 0.5 | 0x0, af + 0.5 | 0x0);
-      if (ah.length < 0xfa && "petalIcons" in window && Object.values(window.petalIcons).length) {
+      i.clearRect(0, 0, canvas.width, canvas.height);
+      ae = ae + 0.01 * (0 - ae);
+      af = af + 0.01 * (0 - af);
+      p(ad, ae + 0.5 | 0, af + 0.5 | 0);
+      if (ah.length < 250 && "petalIcons" in window && Object.values(window.petalIcons).length) {
         let ax = false;
-        for (let ay = 0x0; ay < 0xfa - ah.length; ay++) {
+        for (let ay = 0; ay < 250 - ah.length; ay++) {
           if (Math.random() > 0.9995) {
             let az = Math.random() * (canvas.height + innerHeight);
             ah.push({
-              'id': ai++,
-              'x': -0xa,
-              'y': az,
-              'yy': az,
-              'z': Math.random() * 1.5 + 0x1,
-              'yDir': Math.random() > 0.5 ? 0x1 : -0x1,
-              'speed': 0x1 + Math.random() * 1.5,
-              'spinSpeed': Math.random() * 0.1,
-              'angle': 0x0,
-              'image': at()
+              "id": ai++,
+              "x": -10,
+              "y": az,
+              "yy": az,
+              "z": Math.random() * 1.5 + 1,
+              "yDir": Math.random() > 0.5 ? 1 : -1,
+              "speed": 1 + Math.random() * 1.5,
+              "spinSpeed": Math.random() * 0.1,
+              "angle": 0,
+              "image": at()
             });
             ax = true;
           }
@@ -122,12 +122,12 @@ if (!self.__WB_pmw) {
       }
       for (let bc of ah) {
         bc.x += bc.speed;
-        if (bc.x >= innerWidth + canvas.width + 0x64) {
+        if (bc.x >= innerWidth + canvas.width + 100) {
           ah = ah.filter(bd => bd.id !== bc.id);
           continue;
         }
-        if (Math.abs(bc.y + bc.yDir - bc.yy) > (bc.z + 0x1) / 0x5 * 0x32 * 0.8 * 0x2) {
-          bc.yDir *= -0x1;
+        if (Math.abs(bc.y + bc.yDir - bc.yy) > (bc.z + 1) / 5 * 50 * 0.8 * 2) {
+          bc.yDir *= -1;
         }
         bc.y += bc.yDir * 0.1;
         bc.angle += 0.025 * (bc.speed * 0.667 + bc.spinSpeed);
@@ -137,13 +137,13 @@ if (!self.__WB_pmw) {
         requestAnimationFrame(aw);
       }
     }
-    let bf = 0x32;
+    let bf = 50;
 
     function bg() {
       let bh = b.style.top;
-      bh = +bh.slice(0x0, bh.length - 0x1);
+      bh = +bh.slice(0, bh.length - 1);
       if (Math.abs(bh - bf) > 0.5) {
-        b.style.top = bh + 0.05 * (bf - bh) + '%';
+        b.style.top = bh + 0.05 * (bf - bh) + "%";
       }
     }
     let bi = false;
@@ -155,18 +155,18 @@ if (!self.__WB_pmw) {
         bj = true;
       }
       e.style.display = "none";
-      let bl = localStorage.getItem('lastLogin');
+      let bl = localStorage.getItem("lastLogin");
       if (!bi && (!bl || Date.now() - bl > 86400000)) {
         g.style.display = "block";
         localStorage.removeItem("username");
         localStorage.removeItem("password");
       } else {
-        b = document.getElementById('playButton');
+        b = document.getElementById("playButton");
       }
-      d.style.display = 'block';
-      document.body.classList.add('loaded');
+      d.style.display = "block";
+      document.body.classList.add("loaded");
       setInterval(bg, 16.666666666666668);
-      bf = 0x32;
+      bf = 50;
       aw();
     }
     const bm = (await (await fetch("/changelog.json")).json()).sort(function bn(bo, bp) {
@@ -174,16 +174,16 @@ if (!self.__WB_pmw) {
     });
     bm.forEach(function bq(br, bs) {
       f.innerHTML += "\n            <h1>" + br.name + "</h1>\n            <ul>\n                " + br.changes.map(function bt(bu) {
-        return "<li>" + bu + '</li>';
-      }).join('') + "\n            </ul>\n        ";
-      if (bs !== bm.length - 0x1) {
+        return "<li>" + bu + "</li>";
+      }).join("") + "\n            </ul>\n        ";
+      if (bs !== bm.length - 1) {
         f.innerHTML += "<hr>";
       }
     });
     let bv = false;
 
     function bw() {
-      f.classList.toggle('active');
+      f.classList.toggle("active");
       bv = !bv;
     }
 
@@ -192,25 +192,25 @@ if (!self.__WB_pmw) {
     }
 
     function bz() {
-      let ca = document.getElementById('username').value.trim();
+      let ca = document.getElementById("username").value.trim();
       let cb = document.getElementById("password").value.trim();
       let cc = document.getElementById("infoText");
-      if (ca.length < 0x3 || !/^[a-zA-Z0-9_]+$/.test(ca)) {
+      if (ca.length < 3 || !/^[a-zA-Z0-9_]+$/.test(ca)) {
         cc.innerText = "Invalid username";
         return;
       }
       sessionStorage.setItem("username", ca);
       sessionStorage.setItem("password", cb);
-      bf = -0x64;
+      bf = -100;
       let cd = setInterval(function() {
         let ce = b.style.top;
-        ce = +ce.slice(0x0, ce.length - 0x1);
+        ce = +ce.slice(0, ce.length - 1);
         if (Math.abs(ce - bf) > 0.5) {
           clearInterval(cd);
           bi = true;
           bk();
         }
-      }, 0xfa);
+      }, 250);
     }
     let cf = false;
 
@@ -220,10 +220,10 @@ if (!self.__WB_pmw) {
       }
       cf = true;
       d.style.display = "none";
-      bf = -0x64;
+      bf = -100;
       let ch = setInterval(function() {
         let ci = b.style.top;
-        ci = +ci.slice(0x0, ci.length - 0x1);
+        ci = +ci.slice(0, ci.length - 1);
         if (Math.abs(ci - bf) > 0.5) {
           clearInterval(ch);
           ac = true;
@@ -232,37 +232,37 @@ if (!self.__WB_pmw) {
             bw();
           }
         }
-      }, 0xfa);
+      }, 250);
     }
     Object.defineProperty(window, "toggleChangelog", {
-      'value': bw,
-      'writable': false,
-      'enumerable': false,
-      'configurable': false
+      "value": bw,
+      "writable": false,
+      "enumerable": false,
+      "configurable": false
     });
     Object.defineProperty(window, "toggleSettingsMenu", {
-      'value': bx,
-      'writable': false,
-      'enumerable': false,
-      'configurable': false
+      "value": bx,
+      "writable": false,
+      "enumerable": false,
+      "configurable": false
     });
-    Object.defineProperty(window, 'login', {
-      'value': bz,
-      'writable': false,
-      'enumerable': false,
-      'configurable': false
+    Object.defineProperty(window, "login", {
+      "value": bz,
+      "writable": false,
+      "enumerable": false,
+      "configurable": false
     });
     Object.defineProperty(window, "submit", {
-      'value': cg,
-      'writable': false,
-      'enumerable': false,
-      'configurable': false
+      "value": cg,
+      "writable": false,
+      "enumerable": false,
+      "configurable": false
     });
     Object.defineProperty(window, "loadFunc", {
-      'value': bk,
-      'writable': false,
-      'enumerable': false,
-      'configurable': false
+      "value": bk,
+      "writable": false,
+      "enumerable": false,
+      "configurable": false
     });
     if ("loadScript" in window) {
       window.loadScript();
